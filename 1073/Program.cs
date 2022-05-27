4fd06fc6a54836cc7b5c832eb64a2ca77d225277
guest03 <guest03@CHEINFD14>
2022-05-26 21:33:44 -0300

    Adicionar arquivos de projeto.

---
+++ 1073/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1073
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //definir variaveis
+            int x, v;
+            int[] N = new int[10];
+
+            //ler valor V
+            int.TryParse(Console.ReadLine(), out v);
+            x = v;
+
+             for (int i = 0; i < 10; i++)
+            {
+                N[i] = x;
+                x = x * 2;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                {
+                    Console.WriteLine($"N[{i}] = " + N[i]);
+
+                }
+            }
+        }
+    }
+}
